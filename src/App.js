a2cda01fcb60f724c15806fa9e78b5bc75444a00
Harry Pantaleon <pantaleonhp@gmail.com>
2023-01-21 14:39:10 +0800

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V2.1</h1>
       </header>
     </div>
   )
