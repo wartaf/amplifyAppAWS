6bfe63f751eb4509201c544b60e7d204e452bf10
Harry Pantaleon <pantaleonhp@gmail.com>
2023-01-21 13:01:23 +0800

    new Update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  )
+}
+export default App;
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +40,4 @@ function App() {
 }
 
 export default App;
+*/
